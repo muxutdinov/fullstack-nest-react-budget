bf5a8572b43169e074532691a68042fb961a34a1
muxutdinov <jmuxutdinov@mail.ru>
2023-08-01 17:41:14 +0500

    started and config nest

---
+++ server/src/category/entities/category.entity.ts
@@ -0,0 +1,34 @@
+import { Transaction } from 'src/transaction/entities/transaction.entity';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'category_id' })
+  id: number;
+
+  @Column()
+  title: string;
+
+  @ManyToOne(() => User, (user) => user.categories)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Transaction, (transaction) => transaction.category)
+  @JoinColumn({ name: 'category_id' })
+  transactions: Transaction;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
